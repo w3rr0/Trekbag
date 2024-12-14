563a62559b47a577820c06c1b2dea28b53ae60d4
w3rr0 <konradmateja65@gmail.com>
2024-12-14 18:23:01 +0100

    add sections

---
+++ src/components/AddItemForm.jsx
@@ -0,0 +1,9 @@
+export default function AddItemForm() {
+  return (
+    <form>
+      <h2>Add and item</h2>
+      <input />
+      <button className="btn">Add to list</button>
+    </form>
+  );
+}
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,12 @@
+import AddItemForm from "./AddItemForm";
+import ButtonGroup from "./ButtonGroup";
+
 export default function Sidebar() {
-  return <div>Sidebar</div>;
+  return (
+    <div className="sidebar">
+      <AddItemForm />
+
+      <ButtonGroup />
+    </div>
+  );
 }
