66316d883bce7ba3d8b604f1c31674e50f40e499
Konrad Mateja <konradmateja65@gmail.com>
2024-12-14 19:07:29 +0100

    add all buttons

--- src/components/AddItemForm.jsx
+++ src/components/AddItemForm.jsx
@@ -1,9 +1,11 @@
+import Button from "./Button";
+
 export default function AddItemForm() {
   return (
     <form>
-      <h2>Add and item</h2>
+      <h2>Add an item</h2>
       <input />
-      <button className="btn">Add to list</button>
+      <Button>Add to list</Button>
     </form>
   );
 }
--- src/components/ButtonGroup.jsx
+++ src/components/ButtonGroup.jsx
@@ -1,3 +1,12 @@
+import Button from "./Button";
+
 export default function ButtonGroup() {
-  return <div>ButtonGroup</div>;
+  return (
+    <section className="button-group">
+      <Button type="secondary">Mark all as complete</Button>
+      <Button type="secondary">Mark all as incomplete</Button>
+      <Button type="secondary">Reset to initial</Button>
+      <Button type="secondary">Remove all items</Button>
+    </section>
+  );
 }
