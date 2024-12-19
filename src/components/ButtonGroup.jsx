d4c7a1be89187bb421b195cb84cf507e5fbab8a3
Konrad Mateja <konradmateja65@gmail.com>
2024-12-19 12:31:16 +0100

    change buttons into array

--- src/components/ButtonGroup.jsx
+++ src/components/ButtonGroup.jsx
@@ -1,12 +1,26 @@
 import Button from "./Button";
 
 export default function ButtonGroup() {
+  const secondaryButtons = [
+    "Mark all as complete",
+    "Mark all as incomplete",
+    "Reset to initial",
+    "Remove all items",
+  ];
+
   return (
     <section className="button-group">
-      <Button type="secondary">Mark all as complete</Button>
+      {/* <Button type="secondary">Mark all as complete</Button>
       <Button type="secondary">Mark all as incomplete</Button>
       <Button type="secondary">Reset to initial</Button>
-      <Button type="secondary">Remove all items</Button>
+      <Button type="secondary">Remove all items</Button> */}
+      {secondaryButtons.map((text) => {
+        return (
+          <Button key={text} type="secondary">
+            {text}
+          </Button>
+        );
+      })}
     </section>
   );
 }
