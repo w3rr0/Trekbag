1eb4341a3533869d0810599898f1f1e86ce1e27e
w3rr0 <konradmateja65@gmail.com>
2024-12-28 20:18:21 +0100

    add sorting items

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,18 +1,56 @@
+import Select from "react-select";
 import EmptyView from "./EmptyView";
+import { useState } from "react";
+
+const sortingOptions = [
+  {
+    label: "Sort by default",
+    value: "default",
+  },
+  {
+    label: "Sort by packed",
+    value: "packed",
+  },
+  {
+    label: "Sort by unpacked",
+    value: "unpacked",
+  },
+];
 
 export default function ItemList({
   items,
   handleDeleteItem,
   handleToggleItem,
 }) {
+  const [sortBy, setSortBy] = useState("default");
+
+  const sortedItems = [...items].sort((a, b) => {
+    if (sortBy === "packed") {
+      return b.packed - a.packed;
+    } else if (sortBy === "unpacked") {
+      return a.packed - b.packed;
+    }
+    return;
+  });
+
   return (
     <ul className="item-list">
-      {items.length === 0 && <EmptyView />}
+      {items.length === 0 ? (
+        <EmptyView />
+      ) : (
+        <section className="sorting">
+          <Select
+            onChange={(option) => setSortBy(option.value)}
+            defaultValue={sortingOptions[0]}
+            options={sortingOptions}
+          />
+        </section>
+      )}
 
       {/* <Item item={} />
       <Item item={} />
       <Item item={} /> */}
-      {items.map((item) => {
+      {sortedItems.map((item) => {
         return (
           <Item
             key={item.id}
